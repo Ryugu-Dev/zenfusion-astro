f486249ceb9b32769ce94e26a1225c4c58b22d80
Ryūgū Stūdio <148107491+ryugustudio@users.noreply.github.com>
2024-11-19 18:57:08 +0530

    done with homepage skeleton

--- src/utils/data.js
+++ src/utils/data.js
@@ -23,10 +23,23 @@ export const serviceData = [
 
 export const testimonials = [
     {
-        imgLink: '',
-        quote: '',
-        text: '',
-        cliName: '',
-        cliOffice: ''
+        text: 'ZenFusion made my job search stress-free. They understood my career goals and matched me with a hospital that aligned perfectly with my skills and values. Their support throughout the process was exceptional!',
+        cliName: 'D Kavitha',
+    },
+    {
+        text: 'ZenFusion has been a game-changer for our recruitment process. They consistently provide us with qualified candidates who not only meet our skill requirements but also fit seamlessly into our team culture. Highly recommended!',
+        cliName: 'Priya Menon',
+    },
+    {
+        text: 'As a recent nursing graduate, I struggled to find my first job. ZenFusion guided me through every step, from resume building to interview prep, and helped me land my dream role. I couldn’t be happier!',
+        cliName: 'A Arjun Kumar',
+    },
+    {
+        text: 'ZenFusion helped me find a position that not only matched my skills but also my passion for patient care. The personalized attention they provided was beyond what I expected. A truly rewarding experience!',
+        cliName: 'D Rohit Kumar',
+    },
+    {
+        text: 'We\'ve partnered with ZenFusion multiple times, and they always deliver outstanding candidates. Their understanding of our specific needs and commitment to quality service makes them our go-to consultancy.',
+        cliName: 'Neha Verma',
     }
 ]
