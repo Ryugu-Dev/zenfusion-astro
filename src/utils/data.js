119aca55f7993ef179f88146811713c9453ef45a
Ryūgū Stūdio <148107491+ryugustudio@users.noreply.github.com>
2024-11-19 17:04:29 +0530

    done till testimonials

---
+++ src/utils/data.js
@@ -0,0 +1,32 @@
+export const serviceData = [
+    {
+        imgLink: '/assets/img/all-images/service/service-img1.png',
+        navLink: 'medicalBilling',
+        service: 'Medical Billing'
+    },
+    {
+        imgLink: '/assets/img/all-images/service/service-img1.png',
+        navLink: 'consulting',
+        service: 'Practice Consulting'
+    },
+    {
+        imgLink: '/assets/img/all-images/service/service-img1.png',
+        navLink: 'audits',
+        service: 'Compliance Audits'
+    },
+    {
+        imgLink: '/assets/img/all-images/service/service-img1.png',
+        navLink: 'coding',
+        service: 'Medical Coding'
+    }
+]
+
+export const testimonials = [
+    {
+        imgLink: '',
+        quote: '',
+        text: '',
+        cliName: '',
+        cliOffice: ''
+    }
+]
